7d09c345af141cfb14fcb7180e11dfb00c7891ff
Bruno Coimbra <Bruno Coimbra>
2016-08-13 18:29:24 -0300

    added login authentication

--- app/pages/login/login.ts
+++ app/pages/login/login.ts
@@ -2,14 +2,37 @@ import {Page, Loading} from 'ionic-angular';
 import {NavController} from 'ionic-angular';
 import {Component} from '@angular/core';
 import {HomePage} from '../home/home';
+import {FORM_DIRECTIVES, FormBuilder,  ControlGroup, Validators, AbstractControl} from '@angular/common';
+import {CustomValidators} from '../validators/CustomValidators';
 
 @Page({
-    templateUrl: 'build/pages/login/login.html'
+    templateUrl: 'build/pages/login/login.html',
+    directives: [FORM_DIRECTIVES]
 })
 
 export class LoginPage {
+
+
+    authForm: ControlGroup;
+    username: AbstractControl;
+    password: AbstractControl;
+
     home: string = "home";
-    constructor(public nav: NavController) { }
+    constructor(public nav: NavController, private navController: NavController, private fb: FormBuilder) {
+        this.authForm = fb.group({
+            'username': ['', Validators.compose([Validators.required, Validators.minLength(3), CustomValidators.checkFirstCharacterValidator])],
+            'password': ['', Validators.compose([Validators.required, Validators.minLength(8), CustomValidators.checkFirstCharacterValidator])]
+        });
+
+        this.username = this.authForm.controls['username'];
+        this.password = this.authForm.controls['password'];
+    }
+
+    onSubmit(value: string): void {
+        if(this.authForm.valid) {
+            console.log('Valores enviados: ', value);
+        }
+    }
 
     loginEnter() {
         let loading = Loading.create({
